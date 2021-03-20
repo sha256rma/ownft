bb8549dfef56a6da3cece57bd704c96f9cba3125
Kartikeya Sharma <kartikeyasharma04@gmail.com>
2021-03-19 20:19:17 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import { useEffect } from "react";
 import { Zora } from "@zoralabs/zdk";
-import { Wallet } from "ethers";
+import { Wallet, providers } from "ethers";
+import {
+  constructBidShares,
+  constructMediaData,
+  sha256FromBuffer,
+  generateMetadata,
+} from "@zoralabs/zdk";
 
 import logo from "./logo.svg";
 import "./App.css";
 import getWeb3 from "./getWeb3";
+
+const metadataJSON = generateMetadata("zora-20210101", {
+  description: "",
+  mimeType: "text/plain",
+  name: "",
+  version: "zora-20210101",
+});
 
 function App() {
   useEffect(() => {
@@ -17,6 +30,19 @@ function App() {
         const accounts = await web3.eth.getAccounts();
 
         console.log("account: ", accounts);
+
+        const provider = new providers.Web3Provider(window.ethereum);
+
+        console.log(provider);
+
+        const wallet = Wallet.createRandom();
+        const zora = new Zora(provider, 4);
+
+        console.log("zora", zora);
+
+        const media = await zora.fetchTotalMedia();
+
+        console.log("media: ", media);
       })();
     } catch (error) {
       // Catch any errors for any of the above operations.
@@ -27,6 +53,34 @@ function App() {
     }
   }, []);
 
+  const minting = async () => {
+    const provider = new providers.Web3Provider(window.ethereum);
+    const zora = new Zora(provider, 4);
+
+    const contentHash = sha256FromBuffer(Buffer.from("Ours Truly,"));
+    const metadataHash = sha256FromBuffer(Buffer.from(metadataJSON));
+    const mediaData = constructMediaData(
+      "https://ipfs.io/ipfs/bafybeifyqibqlheu7ij7fwdex4y2pw2wo7eaw2z6lec5zhbxu3cvxul6h4",
+      "https://ipfs.io/ipfs/bafybeifpxcq2hhbzuy2ich3duh7cjk4zk4czjl6ufbpmxep247ugwzsny4",
+      contentHash,
+      metadataHash
+    );
+    /**
+     * Note: Before minting, verify that the content stored at the uris
+     * can be hashed and matches the hashes in the `MediaData`.
+     *
+     * Soon, we will ship utility functions to handle this for you.
+     */
+
+    const bidShares = constructBidShares(
+      10, // creator share
+      90, // owner share
+      0 // prevOwner share
+    );
+    const tx = await zora.mint(mediaData, bidShares);
+    await tx.wait(8); // 8 confirmations to finalize
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -34,6 +88,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={minting}>Mint cryptomedia</button>
       </header>
     </div>
   );
